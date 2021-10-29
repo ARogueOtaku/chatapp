2c03f93ec0d28225139cc6346e372a4f5a76c02d
AntiPrim3 <amit.rockinad.das@gmail.com>
2021-10-29 22:27:03 +0530

    Removed Interpolation of classname in Alert.

--- web/chatapp/src/components/visual/Alert.js
+++ web/chatapp/src/components/visual/Alert.js
@@ -1,8 +1,7 @@
 const Alert = ({ message, color, onClose }) => {
+  const className = `fixed bottom-3 left-1/2 transform -translate-x-1/2 rounded-md py-1 pl-3 text-${color}-700 bg-${color}-200 border border-${color}-700`;
   return (
-    <div
-      className={`fixed bottom-3 mx-auto rounded-md py-1 pl-3 text-${color}-700 bg-${color}-200 border border-${color}-700`}
-    >
+    <div className={className}>
       <span>{message} </span>
       <strong onClick={onClose} className="text-xs font-bold cursor-pointer pr-3 pl-1 py-2">
         X
